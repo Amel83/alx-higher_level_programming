6d90804ed776f535135e6769a7f2277a85902bbc
root <amel83>
2023-06-05 16:39:29 -0700

    c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,25 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * check_cycle - function to check if singly linked list has a cycle
- * @list: head pointer to singly linked list
- * Return: 0 if no cycle, 1 if cycle
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
  */
-int check_cycle(listint_t *list)
+size_t print_listint(const listint_t *h)
 {
-	listint_t *rabbit;
-
-	if (list == NULL)
-		return (0);
-	if (list->next == NULL)
-		return (0);
-	rabbit = list;
-	while (rabbit->next != NULL && rabbit->next->next != NULL)
+	const listint_t *current;
+	unsigned int n;
+	/* number of nodes */
+	current = h;
+	n = 0;
+	while (current != NULL)
 	{
-		list = list->next;
-		rabbit = rabbit->next->next;
-		if (list == rabbit)
-			return (1);
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
 	}
-	return (0);
+	return (n);
+}
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: pointer to a pointer of the start of the list
+ * @n: integer to be included in node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
 }
