443c5a6352bc53479a13f78f65c18a332a27446b
root <amel83>
2023-06-06 18:42:55 -0700

    c

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    def remove_char_at(str, n):
+    copy = ''
+    j = 0
+    for c in str:
+        if j != n:
+            copy += str[j]
+        j += 1
+    return copy
