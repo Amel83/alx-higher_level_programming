fd85b23d03d2b2d0382f4ddfd18181fa106644e5
root <amel83>
2023-06-06 19:21:03 -0700

    c

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 out = ""
-for i in range(0, 100):
-    if i != 99:
-       out += f"{i:02}, "
+for i in range(100):
+    if i < 99:
+       out += "{:02d}, ".format(i)
     else:
         out += "99"
 print(out)
