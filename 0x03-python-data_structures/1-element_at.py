c3a170bdb5e28da78e959901accea5b6e4ab496f
root <amel83>
2023-06-10 12:53:57 -0700

    c

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return
+    if idx > len(my_list) - 1:
+           return 
+    return(my_list[idx])
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+def new_in_list(my_list, idx, element):
+    new_list = my_list
+    if idx < 0:
+        return new_list
+    if idx > (len(new_list) -1):
+        return new_list
+    new_list[idx] = element
+    return new_list
+
