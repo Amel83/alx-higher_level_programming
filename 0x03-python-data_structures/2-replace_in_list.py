da8add0fe074dfbb7447634c6e18abbe9d289912
root <amel83>
2023-06-10 13:41:23 -0700

    c

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
+# 2-replace_in_list.py
+# Tesfanesh Kedir <tesfaneshk222@gmail.com>
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
+    """Replace 1 element"""
+    if idx < #0:
         return my_list
-    if idx > (len(my_list) -1):
+    if idx > (len(my_list) - 1):
         return my_list
     my_list[idx] = element
     return my_list
