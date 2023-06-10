d6bbd47930bb7c832cb86721ab1ed4e1bfb4023e
root <amel83>
2023-06-10 14:03:07 -0700

    c

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-# 2-replace_in_list.py
-# Tesfanesh Kedir <tesfaneshk222@gmail.com>
 def replace_in_list(my_list, idx, element):
-    """Replace 1 element"""
-    if idx < #0:
+    if idx < 0:
         return my_list
     if idx > (len(my_list) - 1):
         return my_list
