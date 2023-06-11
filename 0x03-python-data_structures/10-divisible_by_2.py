5b7eb948f581527f760ec3c42709029d194e9f8f
root <amel83>
2023-06-10 20:58:15 -0700

    js

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     even_no = []
-    for i in range (len(my_list)):
+    for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             even_no.append(True)
         else:
