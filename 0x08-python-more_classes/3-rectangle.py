d7a75da3a86d91335bc202d0a0765640307db0f8
amel83 <tesfaneshk222@gmail.com>
2023-07-05 19:19:18 -0700

    aj

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -48,6 +48,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
     def __str__(self):
         """
             gives the size in #
