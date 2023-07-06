6f99591274cff5832b1a2b969767966b55870f74
amel83 <tesfaneshk222@gmail.com>
2023-07-05 18:18:49 -0700

    a

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 import sys
 
-def is_safe(board, row, col):
-    # Check if the current position is under attack by any previously placed queen
-    for i in range(row):
-        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
+
+def safe(b, r, c):
+    for i in range(r):
+        if board[i] == c or b[i] - i == c - r or b[i] + i == c + r:
             return False
     return True
 
-def solve_nqueens(board, row, n):
-    # Base case: All queens have been placed
-    if row == n:
-        print([[i, board[i]] for i in range(n)])
+def q(b, r, n):
+
+
+    if r == n:
+        print([[i, b[i]] for i in range(n)])
         return
     
-    # Try placing a queen in each column of the current row
     for col in range(n):
-        if is_safe(board, row, col):
-            board[row] = col
-            solve_nqueens(board, row + 1, n)
+        if safe(b, r, c):
+            board[r] = c
+            q(b, r + 1, n)
 
-def nqueens(n):
+def nq(n):
+
+
     if not isinstance(n, int):
         print("N must be a number")
         sys.exit(1)
@@ -28,18 +30,19 @@ def nqueens(n):
         print("N must be at least 4")
         sys.exit(1)
     
-    board = [-1] * n
-    solve_nqueens(board, 0, n)
+    b = [-1] * n
+    q(b, 0, n)
 
 if __name__ == "__main__":
+
+
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
     
     try:
         n = int(sys.argv[1])
-        nqueens(n)
+        q(n)
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
