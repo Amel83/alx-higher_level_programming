d2ec7b8207539369829f9bcced1967d4736e655d
amel83 <tesfaneshk222@gmail.com>
2023-07-05 18:22:31 -0700

    a

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,48 +1,62 @@
 #!/usr/bin/python3
 import sys
 
-
-def safe(b, r, c):
-    for i in range(r):
-        if board[i] == c or b[i] - i == c - r or b[i] + i == c + r:
+def is_safe(board, row, col):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i] == col:
             return False
+
+    # Check if there is a queen in the upper left diagonal
+    i = row - 1
+    j = col - 1
+    while i >= 0 and j >= 0:
+        if board[i] == j:
+            return False
+        i -= 1
+        j -= 1
+
+    # Check if there is a queen in the upper right diagonal
+    i = row - 1
+    j = col + 1
+    while i >= 0 and j < len(board):
+        if board[i] == j:
+            return False
+        i -= 1
+        j += 1
+
     return True
 
-def q(b, r, n):
-
-
-    if r == n:
-        print([[i, b[i]] for i in range(n)])
+def solve_nqueens(board, row):
+    if row == len(board):
+        print(board)
         return
-    
-    for col in range(n):
-        if safe(b, r, c):
-            board[r] = c
-            q(b, r + 1, n)
 
-def nq(n):
+    for col in range(len(board)):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens(board, row + 1)
 
-
-    if not isinstance(n, int):
+def nqueens(N):
+    if not isinstance(N, int):
         print("N must be a number")
         sys.exit(1)
-    if n < 4:
+
+    if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
-    b = [-1] * n
-    q(b, 0, n)
 
-if __name__ == "__main__":
+    board = [-1] * N
+    solve_nqueens(board, 0)
 
-
+if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
-        n = int(sys.argv[1])
-        q(n)
+        N = int(sys.argv[1])
+        nqueens(N)
     except ValueError:
         print("N must be a number")
         sys.exit(1)
