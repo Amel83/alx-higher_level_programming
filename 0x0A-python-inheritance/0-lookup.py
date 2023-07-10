6e79349d1ee385e7c8dc108ee30a2a27094aa505
amel83 <tesfaneshk222@gmail.com>
2023-07-10 16:30:42 -0700

    sj

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" question 0"""
+def lookup(obj):
+    """ return list of available attributes and methods of an object:"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""inherit from list"""
+
+
+class MyList(list):
+    """ inherit"""
+
+    def print_sorted(self):
+        """print sorted list"""
+        return sorted(self)
