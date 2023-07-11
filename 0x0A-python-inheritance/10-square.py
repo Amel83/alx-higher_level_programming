f0911adcd1842c5dd80fb9e8a4903d198d3092f8
amel83 <tesfaneshk222@gmail.com>
2023-07-10 19:03:58 -0700

    aj

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,7 +9,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        ""cheque value"""
+        """cheque value"""
         x = type(value)
         if x != int:
             raise TypeError("{} must be an integer".format(name))
