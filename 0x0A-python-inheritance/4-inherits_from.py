762f6f6df83a93824e21ee8bb188aa116ead3232
amel83 <tesfaneshk222@gmail.com>
2023-07-10 18:57:10 -0700

    aj

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """check if object is a class that
-inherited from specified class"""
+inherited """
 
 
 def inherits_from(obj, a_class):
     """return True if obj is a class that
-    inherited from specified class"""
+    inherited """
     if issubclass(obj.__class__, a_class) is True:
         if obj.__class__ is not a_class:
             return True
