e0f865ea408401bf02998bbdb8e81171caf8a72e
amel83 <tesfaneshk222@gmail.com>
2023-07-10 19:08:09 -0700

    ai

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,12 +9,13 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        ""cheque value"""
+        """cheque value"""
         x = type(value)
         if x != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
     """class Rectangle inherits BaseGeometry"""
