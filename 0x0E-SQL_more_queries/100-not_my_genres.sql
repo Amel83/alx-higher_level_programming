6c296f0777e29db35c9d41bac8004647c37b296a
amel83 <tesfaneshk222@gmail.com>
2023-08-16 18:01:22 -0700

    ao

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,7 @@
+--  script that uses the database to list all genres not linked to the show Dexter
+SELECT tv_genres.name
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title != 'Dexter' OR tv_shows.title IS NULL
+ORDER BY tv_genres.name ASC;
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Create the table if it doesn't exist
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS 'force_name' (
+    'id' INT,
+    'name' VARCHAR(256) NOT NULL
 );
