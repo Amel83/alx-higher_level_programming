3a466ed1f6e0ce9aa66f921d95d9478abff63900
amel83 <tesfaneshk222@gmail.com>
2023-08-17 01:42:44 -0700

    ao

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- GET SCORE GREATER THATN 10
-SELECT score, name FROM second_table where score >= 10
-order by score DESC;
+SELECT 'score', 'name' FROM 'second_table' where 'score' >= 10
+order by 'score' DESC;
