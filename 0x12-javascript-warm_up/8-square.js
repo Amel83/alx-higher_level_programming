a76b268970b63662924554f923ae601e4d701f97
amel83 <tesfaneshk222@gmail.com>
2023-09-13 06:27:52 -0700

    wo

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+!/usr/bin/node
+if (isNaN size)
+	console.log(sizs is missing);
+let b;
+else{
+	for (b = 0; b < size; r++)
+	{
+		let row = '';
+		for (c = 0; c < size; c++) row += 'X'
+		console.log(row);
+	}
+}
