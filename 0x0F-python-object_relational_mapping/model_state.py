acb5c9b295546e2713d5742257359c93765bc98c
amel83 <tesfaneshk222@gmail.com>
2023-09-16 05:15:00 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,10 +2,38 @@
 """Start link class to table in database
 """
 import sys
-from model_state import Base, State
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy import (create_engine)
+Base = declarative_base()
+
+class State(Base):
+    """State class."""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Create the engine to connect to the MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create the table in the database
     Base.metadata.create_all(engine)
+
+    # Create a Session class that will be used to interact with the database
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # You can perform database operations using the 'session' object
+
+    # Close the session when done
+    session.close()
