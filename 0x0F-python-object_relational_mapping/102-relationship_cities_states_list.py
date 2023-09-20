e5cc08cb3572142fde699e4d406de70cc2e1b775
amel83 <tesfaneshk222@gmail.com>
2023-09-20 16:40:05 -0700

    edh

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""
-Lists all City objects from the database hbtn_0e_101_usa along with their corresponding State objects.
+""" prints the State object with the name passed as argument from the database
 """
 import sys
-from sqlalchemy import create_engine
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from sqlalchemy.orm import relationship
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    try:
-        cities = session.query(City).order_by(City.id).all()
-        for city in cities:
-            state_name = city.state.name
-            print("{}: {} -> {}".format(city.id, city.name, state_name))
-    except Exception as e:
-        print("Error:", e)
-    finally:
-        session.close()
+    for instance in session.query(State).order_by(State.id):
+        for city_ins in instance.cities:
+            print(city_ins.id, city_ins.name, sep=": ", end="")
+            print(" -> " + instance.name)
