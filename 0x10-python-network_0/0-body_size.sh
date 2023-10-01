1126157dbee9565bfbaea53c8b8395fe15e2c752
amel83 <tesfaneshk222@gmail.com>
2023-10-01 00:41:02 -0700

    sfi

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-url="$1"
-response=$(curl -sI "$url")
-content_length=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r')
-if [ -n "$content_length" ]; then
-    echo "$content_length"
-else
-    echo "Content-Length header not found in the response."
-fi
+# HTTP response header for given URL
+curl -s "$1" | wc -c
