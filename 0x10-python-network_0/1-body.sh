6d123b4979c22075f6d2c6e44286801ac5c00521
amel83 <tesfaneshk222@gmail.com>
2023-10-01 02:14:43 -0700

    grbh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-
-# Check if a URL argument is provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url="$1"
-
-# Use curl to send a GET request to the URL, follow redirects, and store the response in a variable
-response=$(curl -sL "$url")
-
-# Get the HTTP status code from the response
-http_status=$(echo "$response" | head -n 1 | awk '{print $2}')
-
-# Check if the status code is 200 (OK)
-if [ "$http_status" -eq 200 ]; then
-    # Extract and display the body of the response
-    echo "$response" | sed '1,/^$/d'
-else
-    echo "HTTP Status Code: $http_status"
-fi
+#  takes in a URL, sends a GET request to the URL
+curl -sL "$1"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-# Check if a URL argument is provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url="$1"
-
-# Use curl to send a GET request to the URL with the X-School-User-Id header and store the response in a variable
-response=$(curl -s -H "X-School-User-Id: 98" "$url")
-
-# Display the body of the response
-echo "$response"
+# send a get request to a URL 
+curl -sH "X-School-User-Id: 98" "${1}"
