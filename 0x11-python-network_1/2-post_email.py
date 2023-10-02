96c08e69dae52653d42e704d6636a4eebe63ffd9
amel83 <tesfaneshk222@gmail.com>
2023-10-01 17:07:58 -0700

    sfg

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""A script that  takes in a URL
+"""A script that takes in a URL
 """
-import sys
-import urllib.parse
 import urllib.request
+import urllib.parse
+import sys
 
 
-if __name__ == "__main__":
-    u = sys.argv[1]
-    v = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(v).encode("ascii")
+url = sys.argv[1]
+email = sys.argv[2]
 
-request = urllib.request.Request(u, data)
-with urllib.request.urlopen(request) as response:
-    print(response.read().decode("utf-8"))
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+req = urllib.request.Request(url, data=data, method='POST')
+
+try:
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print("Your email is:", email)
+        print(body)
+except Exception as e:
+    print("Error:", e)
+-
