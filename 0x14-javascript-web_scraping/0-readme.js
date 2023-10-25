62fa7f9b3190a67929e57c19671cb629081854c7
amel83 <tesfaneshk222@gmail.com>
2023-10-25 01:37:19 -0700

    555

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const fss = require('fs');
+const fs = require('fs');
 const file = process.argv[2];
 
-fss.readFile(file, 'utf-8', function (err, data) {
+fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
